b6e345732147ffa1ef7cf04d4b514c424c6d30f1
Lindsay <lindnerdesign@gmail.com>
2018-08-05 22:32:11 -0500

    database

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+-- Create the database wishes_db and specified it for use.
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create the table wishes.
+CREATE TABLE burgers
+(
+id INT NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(100) NOT NULL,
+devoured BOOLEAN
+PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Elk Burger');
+INSERT INTO burgers (burger_name) VALUES ('California Burger');
+INSERT INTO burgers (burger_name) VALUES ('Pizza Burger');
+INSERT INTO burgers (burger_name) VALUES ('Juicy Lucy Burger');
